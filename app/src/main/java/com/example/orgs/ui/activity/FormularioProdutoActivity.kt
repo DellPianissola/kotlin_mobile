75cb83b442fe8480065b11a0664a9b0b6d605a8c
Gabriel Pianissola <gabriel.pianissola@codex.com.br>
2025-09-04 08:19:56 -0300

    Modificações efetuadas em deploymentTargetSelector.xml, build.gradle.kts e 11 outros arquivos...

---
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,33 @@
+package com.example.orgs.ui.activity
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.orgs.R
+import com.example.orgs.module.Produto
+import java.math.BigDecimal
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val descricao = campoDescricao.text.toString()
+            val campoValor = findViewById<EditText>(R.id.valor)
+            val valorEmtexto = campoValor.text.toString()
+            val valor = if (valorEmtexto.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(valorEmtexto)
+            }
+
+            Produto(nome, descricao, valor)
+        }
+    }
+}
