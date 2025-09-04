dce95174bd424bee5525296306befaa780566f61
Gabriel Pianissola <gabriel.pianissola@codex.com.br>
2025-09-04 15:00:23 -0300

    Binding

--- app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
@@ -3,45 +3,45 @@ package com.example.orgs.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import com.example.orgs.R
 import com.example.orgs.dao.ProdutosDao
+import com.example.orgs.databinding.ActivityListProdutosBinding
 import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-
-class ListaProdutosActivity : AppCompatActivity(R.layout.activity_list_produtos) {
+class ListaProdutosActivity : AppCompatActivity() {
 
     private val dao = ProdutosDao()
     private val adapter = ListaProdutosAdapter(this, dao.buscaTodos())
-//    private val binding by lazy {
-//        ActivityListaProdutosActivityBinding.inflate()
-//    }
+
+    // inicialização do view binding
+    private lateinit var binding: ActivityListProdutosBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        configuraRecylcerView()
+
+        // inflando o layout com binding
+        binding = ActivityListProdutosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configuraRecyclerView()
+        configuraFab()
     }
 
     override fun onResume() {
         super.onResume()
         adapter.atualiza(dao.buscaTodos())
-        configuraFab()
     }
 
-    private fun configuraRecylcerView() {
-        val recyclerView = findViewById<RecyclerView>(R.id.activity_lista_produto_recyclerView)
-        recyclerView.adapter = adapter
+    private fun configuraRecyclerView() {
+        binding.activityListaProdutoRecyclerView.adapter = adapter
     }
 
     private fun configuraFab() {
-        val fab = findViewById<FloatingActionButton>(R.id.activity_lista_produto_fab)
-        fab.setOnClickListener {
-            vai_para_formulario_produto()
+        binding.activityListaProdutoFab.setOnClickListener {
+            vaiParaFormularioProduto()
         }
     }
 
-    private fun vai_para_formulario_produto() {
+    private fun vaiParaFormularioProduto() {
         val intent = Intent(this, FormularioProdutoActivity::class.java)
         startActivity(intent)
     }
